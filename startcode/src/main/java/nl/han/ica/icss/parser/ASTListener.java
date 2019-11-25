81b8ee9d156ed7242153bf876af68344dcea6a06
Joey Stoffels <joey.stoffels@exe.energy>
2019-11-25 21:37:24 +0100

    first commit

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -2,6 +2,9 @@ package nl.han.ica.icss.parser;
 
 import java.util.Stack;
 import nl.han.ica.icss.ast.*;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 /**
  * This class extracts the ICSS Abstract Syntax Tree from the Antlr Parse tree.
@@ -21,4 +24,27 @@ public class ASTListener extends ICSSBaseListener {
     public AST getAST() {
         return ast;
     }
+
+    @Override
+	public void enterStylesheet(ICSSParser.StylesheetContext ctx) {
+		this.ast.setRoot(new Stylesheet());
+		System.out.println("Test1");
+	}
+
+	@Override
+	public void enterEveryRule(ParserRuleContext ctx) {
+		System.out.println("Test2");
+
+		super.enterEveryRule(ctx);
+	}
+
+	@Override
+	public void visitTerminal(TerminalNode node) {
+		super.visitTerminal(node);
+	}
+
+	@Override
+	public void visitErrorNode(ErrorNode node) {
+		super.visitErrorNode(node);
+	}
 }
